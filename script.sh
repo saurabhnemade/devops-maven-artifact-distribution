57e56d268a1ddf492b728b806578fcb7e632d240
saurabhnemade <saurabhnemade@users.noreply.github.com>
2018-08-29 00:30:05 +0530

    Updated auto incrementing pom snapshot version

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
-#Deploying Snapshots
+# Deploying Snapshots
 mvn --settings settings.xml deploy
 
 
-#Deploying Release Version
+# Deploying Release Version
+
 mvn versions:set -DremoveSnapshot
 mvn versions:commit
 mvn --settings settings.xml deploy
+
+# Auto Increment Snapshot versions
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
